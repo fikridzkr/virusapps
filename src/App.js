5e886202b7d2d2854f07e6de47053a9cf574f2e5
Fikri Dzakir <fikridzakir179@gmail.com>
2021-02-11 00:02:22 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Navigation from "./components/Navigation";
+import { Row, Col, Button } from "react-bootstrap";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <div className="mt-4">
+        <h1>Data Sebaran</h1>
+        <Row className="m-4">
+          <Col md={3} className="bg-danger p-4 text-left ">
+            <h4 className="mb-2">Global</h4>
+            <hr className="m-1"></hr>
+            <div>
+              <span>Positif</span>
+              <br />
+              <span>
+                <strong>11299921</strong>
+              </span>
+            </div>
+            <div>
+              <span>Sembuh</span>
+              <br />
+              <span>
+                <strong>11299921</strong>
+              </span>
+            </div>
+            <div>
+              <span>Meninggal</span>
+              <br />
+              <span>
+                <strong>11299921</strong>
+              </span>
+            </div>
+          </Col>
+          <Col md={3} className="bg-success p-4 text-left">
+            <h4 className="mb-2">Indonesia</h4>
+            <hr className="m-1"></hr>
+            <div>
+              <span>Positif</span>
+              <br />
+              <span>
+                <strong>11299921</strong>
+              </span>
+            </div>
+            <div>
+              <span>Sembuh</span>
+              <br />
+              <span>
+                <strong>11299921</strong>
+              </span>
+            </div>
+            <div>
+              <span>Meninggal</span>
+              <br />
+              <span>
+                <strong>11299921</strong>
+              </span>
+            </div>
+          </Col>
+          <Col md={6} className="py-5 images img-fluid">
+            <div className="mh-100 pl-4 row flex-column justify-content-center align-items-start">
+              <h4>
+                Situasi Virus COVID-19 <br />
+                Di indonesia
+              </h4>
+              <Button>Simak Tayangan Data Langsung</Button>
+            </div>
+          </Col>
+          <Col className="my-3">
+            <p>tanggal hari ini</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col></Col>
+        </Row>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+const Navigation = () => {
+  return (
+    <Navbar bg="danger" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Kawal Corona</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Blog</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Navigation;
